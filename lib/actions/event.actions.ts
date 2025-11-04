ccb1a64d472bf95f16c328ac88be733be74852b3
LaurentBarres <yoannpedase59@outlook.com>
2025-11-04 16:31:20 +0100

    add simila event correction

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -1,17 +1,15 @@
 'use server';
 
-import Event, { IEvent } from '@/database/event.model';
+import Event from '@/database/event.model';
 import connectDB from "@/lib/mongodb";
 
-export const getSimilarEventsBySlug = async (slug: string): Promise<IEvent[]> => {
+export const getSimilarEventsBySlug = async (slug: string) => {
     try {
         await connectDB();
         const event = await Event.findOne({ slug });
-        if (!event) return [] as IEvent[];
 
-        // Return Mongoose documents to match IEvent (which extends Document)
-        return await Event.find({ _id: { $ne: event._id }, tags: { $in: event.tags } });
+        return await Event.find({ _id: { $ne: event._id }, tags: { $in: event.tags } }).lean();
     } catch {
-        return [] as IEvent[];
+        return [];
     }
 }
